f545d52e0071da3969dfd553d1eaeda17eece4f4
raoulfomo <fomoraange@gmail.com>
2024-11-22 15:05:25 +0100

    copy files project

---
+++ android/app/src/main/kotlin/com/eevministry/learnapp/MainActivity.kt
@@ -0,0 +1,9 @@
+package com.eevministry.learnapp
+
+import io.flutter.embedding.android.FlutterActivity
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/common/utils/constants.dart
@@ -0,0 +1,7 @@
+class Constants {
+  
+  static const dommain = 'https://esprit-vie.com';
+  static const baseUrl = '$dommain/api/development/';
+  static const apiKey = '1234';
+  
+}
